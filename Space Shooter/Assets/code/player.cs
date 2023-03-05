79b0d26a100bbcb7169ebec74c4347c1e0b0f0b6
algoritmika14 <algomklass@gmail.com>
2023-03-05 15:18:28 +0500

    1

---
+++ Space Shooter/Assets/code/player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 realPos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = realPos;
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+}
---
+++ clicer/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 targetPosition;
+    public float moveStep;
+
+    public class Square:
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
